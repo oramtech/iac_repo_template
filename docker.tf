d42d641ecb0ea9e6f379366550c493306c774e1b
Ben Oram <b@oram.co>
2023-01-10 08:26:56 -0700

    Added example docker.tf file

---
+++ docker.tf
@@ -0,0 +1,44 @@
+variable "dockertcp_host" { 
+  type = string
+}
+
+variable "dockertcp_ca_material" { 
+  type = string
+}
+
+variable "dockertcp_cert_material" { 
+  type = string
+}
+
+variable "dockertcp_key_material" { 
+  type = string
+}
+
+# resource "docker_image" "cloudflared" {
+#   name = "cloudflare/cloudflared:latest"
+# }
+
+
+# # Create a container
+# resource "docker_container" "cloudflared" {
+#   image = docker_image.cloudflared.image_id
+#   name  = "cloudflared_tunnel"
+#   restart = "always"
+#   command = ["tunnel", "--no-autoupdate", "run", "--token", "${var.cloudflared_token}"]
+# }
+
+
+# resource "docker_image" "nginx" {
+#   name = "nginx:latest"
+# }
+
+# resource "docker_container" "testnginx" {
+#   image = docker_image.nginx.image_id
+#   name  = "test_nginx_cloudflare_app"
+#   restart = "always"
+#   ports {
+#     internal = "80"
+#     external = "8089"
+#   }
+# }
+
